468e0d0a4cd35160ad1d6eca88ef21295010c7f2
Rutendo Musuka <70662623+TendoMusuka@users.noreply.github.com>
2022-02-09 10:48:21 -0800

    Add files via upload

--- IntrotoJava/FirstProgram.java
+++ IntrotoJava/FirstProgram.java
@@ -1,8 +1,9 @@
+/**Rutendo Musuka
+ * Udemy Java Bootcamps Notes, Workshops and Challenges 
+ */
+
 // TASK 1: Make a class.
-
 // TASK 2: Inside the class, write the main() method.
-
-
 // TASK 3: Print these messages to the console: "Printing is fun!" 
                                               // "Java > Python."
                                               // "I spilled Java all over my paper."
