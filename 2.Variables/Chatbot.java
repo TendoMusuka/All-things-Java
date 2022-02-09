9f6cc7e3e7bcc8b3729a95a976c98e8316046e13
Rutendo Musuka <70662623+TendoMusuka@users.noreply.github.com>
2022-02-09 11:32:13 -0800

    Update Chatbot.java

--- 2.Variables/Chatbot.java
+++ 2.Variables/Chatbot.java
@@ -1,35 +1,43 @@
-//import Scanner
+/**Rutendo Musuka's code
+ * Udemy Java Bootcamps Notes, Workshops and Challenges 
+ */
+
+import java.util.Scanner; //import Scanner
 
 public class Chatbot {
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+
         System.out.println("Hello. What is your name?");
-        //Pick up user's name and store it. 
+        String name = scan.nextLine(); //Pick up user's name and store it. 
 
                            
-        //add new a line before asking next question. 
-        System.out.println("Hi <name>! I'm Javabot. Where are you from?");
-        //Pick up user's home and store it. 
+        System.out.println(""); //add new a line before asking next question.  
+        //Having println and \n prints two empty lines
+        System.out.println("Hi " + name + "! I'm Javabot. Where are you from?");
+        String home = scan.nextLine(); //Pick up user's home and store it. 
                         
 
-        //add new a line before asking next question.
-        System.out.println("I hear it's beautiful at <home>! I'm from a place called Oracle");
+        System.out.println("");//add new a line before asking next question.
+        System.out.println("I hear it's beautiful at " + home + "I'm from a place called Oracle");
         System.out.println("How old are you?");
-        //Pick up age and store it.
+        double age = scan.nextInt();
 
+        scan.nextLine(); 
 
-        //add new a line before asking next question.
-        System.out.println("So you're <age>, cool! I'm 400 years old.");
-        System.out.println("This means I'm <divide 400 by age> times older than you.");
+        System.out.println(" "); //add new a line before asking next question.
+        System.out.println("So you're "+ age + ". Cool! I'm 400 years old.");
+        System.out.println("This means I'm " + (int)(400/age) + " times older than you."); //casted it to make it look neater
+
+        System.out.println(" "); 
         System.out.println("Enough about me. What's your favourite language? (just don't say Python)");
-        //Pick up language and store it. 
+        String language = scan.nextLine(); //Pick up language and store it. 
 
 
-        //add new a line here.
-        System.out.println("<language>, that's great! Nice chatting with you <name>. I have to log off now. See ya!");
-        
-        //close scanner. 
+        System.out.println(" ");//add new a line here.
+        System.out.println(language + ", that's great! Nice chatting with you," + name + ". I have to log off now. See ya!");
+    
+        scan.close();//close scanner. 
 
-        
     }
 }
