a0b170cca7a3a3c17861dc77b54a883b628677a7
Rutendo Musuka <70662623+TendoMusuka@users.noreply.github.com>
2022-02-28 13:58:09 -0800

    Add files via upload

---
+++ 9. Unit Testing_and_Streams/static/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+  
+    public static void main(String[] args) {
+
+      Employee zack = new Employee( "Zack", " Programmer");
+      Employee gio = new Employee ("Gio", "Analyst");
+      Employee thomas = new Employee("Thomas", "Specialist")  ;
+      System.out.println(Employee.getHiredCount());
+    }
+  
+}
