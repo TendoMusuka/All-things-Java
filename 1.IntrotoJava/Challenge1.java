b49cf5209bd830491dbe01f6bdbe6cf7ef3da9c5
Rutendo Musuka <70662623+TendoMusuka@users.noreply.github.com>
2022-03-07 22:20:52 -0800

    Add files via upload

---
+++ 1.IntrotoJava/Challenge1.java
@@ -0,0 +1,18 @@
+//Challenge 1 Java Course with notes 
+
+public class Challenge1 {
+
+    public static void main(String[] args) {
+        System.out.println("RRRRRRRR      M               M");  //System.out.print does not automatically print to a new lin
+        System.out.println("R      R      M   M       M   M");  //System.out.print("R      R      M   M       M   M /n");
+        System.out.println("RRRRRRRR      M    M     M    M"); // /n - prints a new line so the above is an alternative 
+        System.out.println("R             M       M       M");
+        System.out.println("R  R          M               M");
+        System.out.println("R    R        M               M");
+        System.out.println("R      R      M               M");
+        System.out.println("R        R    M               M");
+        System.out.println("");
+    }
+}
+
+//Regularly compile and run to test your code so you know what to fix 
