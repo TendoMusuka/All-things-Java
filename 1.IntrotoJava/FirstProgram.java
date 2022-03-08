da3b48f6086092b14f05153aa8d7cd7875ed2614
Rutendo Musuka <70662623+TendoMusuka@users.noreply.github.com>
2022-03-07 22:21:30 -0800

    Add files via upload

---
+++ 1.IntrotoJava/FirstProgram.java
@@ -0,0 +1,19 @@
+/**Rutendo Musuka
+ * Udemy Java Bootcamps Notes, Workshops and Challenges 
+ */
+
+// TASK 1: Make a class.
+// TASK 2: Inside the class, write the main() method.
+// TASK 3: Print these messages to the console: "Printing is fun!" 
+                                              // "Java > Python."
+                                              // "I spilled Java all over my paper."
+                                              // "My dog ate my Java." 
+public class FirstProgram {
+
+    public static void main(String[] args) {
+        System.out.println("Printing is fun!") ;
+        System.out.println("Java > Python.") ;
+        System.out.println("I spilled Java all over my paper.") ;
+        System.out.println("My dog ate my Java."); 
+    }
+}
