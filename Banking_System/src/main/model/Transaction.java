b978f0d9610f0e9fc8feacc5a7b810787e5289d4
Rutendo Musuka <70662623+TendoMusuka@users.noreply.github.com>
2022-03-11 23:18:11 -0800

    Add files via upload

--- Banking_System/src/main/model/Transaction.java
+++ Banking_System/src/main/model/Transaction.java
@@ -1,7 +1,65 @@
 package src.main.model;
 
+import src.main.model.account.*;
+
 public class Transaction {
 
-  
+    Account type;
+    long timestamp;
+    String id;
+    double amount;
+
+    //CONSTRUCTOR 
+    public Transaction(Account type, long timestamp, String id, double amount) {
+        this.type = type;
+        this.timestamp = timestamp;
+        this.id = id;
+        this.amount = amount;
+    }
+
+    //COPY CONSTRUCTORS 
+    public Transaction(Transaction source) {
+        this.type = source.type;
+        this.timestamp = source.timestamp;
+        this.id = source.id;
+        this.amount = source.amount;
+    }
+
+
+    //COPY CONSTRUCTOR 
+
+    //GETTERS AND SETTERS 
+    public Account getType() {
+        return this.type;
+    }
+
+    public void setType(Account type) {
+        this.type = type;
+    }
+
+    public long getTimestamp() {
+        return this.timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public double getAmount() {
+        return this.amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+    
 
 }
